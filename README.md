a3e3461cdbc25b0695e33ae460f5cb6a88de818c
pankaj <er.pankaj,k4@gmail.com>
2023-09-19 03:21:11 +0530

    Ready to uplaod to github

--- README.md
+++ README.md
@@ -1,70 +1,58 @@
-# Getting Started with Create React App
+# React Calculator App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a simple calculator application built using React and React Hooks. It allows users to perform basic arithmetic operations, view the calculation history, previous result and clear inputs.
 
-## Available Scripts
+## Hosted Link on Netlify
+ - Click the Link to Preview <br/>
 
-In the project directory, you can run:
+## Table of Contents
+- [Demo](#demo)
+- [Features](#features)
+- [Getting Started](#getting-started)
 
-### `npm start`
+## Demo
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Features
+- Addition, subtraction, multiplication, and division operations.
+- Percentage calculation.
+- Clear All (AC) and Clear Entry (CE) buttons.
+- Responsive design for various screen sizes.
+- Real-time calculation display.
+- History of calculations.
+- Previous Result of calculation.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Technologies Used
+- React
+- React Hooks
+- HTML
+- CSS
+- JavaScript
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Getting Started
 
-### `npm run build`
+Follow these steps to set up and run the React Calculator App on your local machine.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+1. Clone the repository:
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+   ```
+   git clone 
+   cd react-calculator
+   npm install
+   ```
+2. Install the dependencies:
+```
+npm install
+```
+3. Start the server:
+```
+npm start
+```
+The application should now be running at `http://localhost:3000`.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,184 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    // Define and initialize state variables
+  const [display1, setDisplay1] = useState(''); // To display calculations
+  const [display2, setDisplay2] = useState('0'); // To display the result
+  const [tempResult, setTempResult] = useState('0'); // To display the previous result
+  const [currentInput, setCurrentInput] = useState(''); // Current user input
+  const [result, setResult] = useState(null); // Store the result of calculations
+
+  // Function to handle number button clicks
+  const handleNumberClick = (num) => {
+    setCurrentInput((prevInput) => prevInput + num);
+    setDisplay2(currentInput);
+  };
+
+   // Function to handle operation button clicks
+  const handleOperationClick = (operator) => {
+    if (result) {
+      setDisplay1(currentInput + operator);
+      setCurrentInput('');
+      setDisplay2('');
+    }
+    else if (currentInput) {
+      setDisplay1((prevDisplay1) => prevDisplay1 + currentInput + operator);
+      let n = eval(display1 + currentInput).toString();
+      setTempResult(n);
+      setCurrentInput('');
+    }
+    setResult(null);
+  };
+
+ // Function to handle decimal dot button click
+  const handleDotClick = () => {
+    if (currentInput.indexOf('.') === -1) {
+      setCurrentInput((prevInput) => prevInput + '.');
+    }
+  };
+
+    // Function to handle "AC" (All Clear) button click
+  const handleAllClear = () => {
+    setDisplay1('');
+    setDisplay2('0');
+    setTempResult('');
+    setCurrentInput('');
+  };
+
+ // Function to handle "CE" (Last Entity Clear) button click
+  const handleLastEntityClear = () => {
+    setDisplay2('0');
+    setCurrentInput('');
+  };
+
+    // Function to handle "=" (Equals) button click
+  const handleEquals = () => {
+    if (currentInput === ".") return;
+
+    if (currentInput) {
+      setDisplay1((prevDisplay1) => prevDisplay1 + currentInput);
+      const result = eval(display1 + currentInput).toString();
+      setDisplay2(result);
+      setCurrentInput(result);
+      setResult(result);
+      setTempResult('');
+    }
+  };
+
+  // console.log("display1 :", display1);
+  // console.log("display2 :", display2);
+  // console.log("tempResult :", tempResult);
+  // console.log("currentInput :", currentInput);
+  // console.log("___________________");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Calculator Using React Hooks</h1>
       </header>
+      <section>
+        <div className="display">
+          <div className="display-1">{display1 || 0}</div>
+          <div className="display-2">{currentInput || display2}</div>
+          <div className="temp-result">{tempResult}</div>
+        </div>
+
+        {/* Calculator buttons */}
+        <div className="container">
+          {/* Rows of buttons */}
+          <div className="row">
+            <button className="btn btn-2 all-clear" onClick={handleAllClear}>
+              AC
+            </button>
+            <button
+              className="btn btn-1 operation"
+              onClick={() => handleOperationClick('%')}
+            >
+              %
+            </button>
+            <button
+              className="btn btn-1 operation"
+              onClick={() => handleOperationClick('/')}
+            >
+              /
+            </button>
+          </div>
+          {/* Rows of buttons */}
+          <div className="row">
+            <button className="btn btn-1 number" onClick={() => handleNumberClick('7')}>
+              7
+            </button>
+            <button className="btn btn-1 number" onClick={() => handleNumberClick('8')}>
+              8
+            </button>
+            <button className="btn btn-1 number" onClick={() => handleNumberClick('9')}>
+              9
+            </button>
+            <button
+              className="btn btn-1 operation"
+              onClick={() => handleOperationClick('*')}
+            >
+              X
+            </button>
+          </div>
+          {/* Rows of buttons */}
+          <div className="row">
+            <button className="btn btn-1 number" onClick={() => handleNumberClick('4')}>
+              4
+            </button>
+            <button className="btn btn-1 number" onClick={() => handleNumberClick('5')}>
+              5
+            </button>
+            <button className="btn btn-1 number" onClick={() => handleNumberClick('6')}>
+              6
+            </button>
+            <button
+              className="btn btn-1 operation"
+              onClick={() => handleOperationClick('-')}
+            >
+              -
+            </button>
+          </div>
+          {/* Rows of buttons */}
+          <div className="row">
+            <button className="btn btn-1 number" onClick={() => handleNumberClick('1')}>
+              1
+            </button>
+            <button className="btn btn-1 number" onClick={() => handleNumberClick('2')}>
+              2
+            </button>
+            <button className="btn btn-1 number" onClick={() => handleNumberClick('3')}>
+              3
+            </button>
+            <button
+              className="btn btn-1 operation"
+              onClick={() => handleOperationClick('+')}
+            >
+              +
+            </button>
+          </div>
+          {/* Rows of buttons */}
+          <div className="row">
+            <button className="btn btn-1 number" onClick={() => handleNumberClick('0')}>
+              0
+            </button>
+            <button className="btn btn-1 dot" onClick={handleDotClick}>
+              .
+            </button>
+            <button className="btn btn-2 equals" onClick={handleEquals}>
+              =
+            </button>
+          </div>
+        </div>
+        <button
+          className="btn btn-2 last-entity-clear"
+          style={{ width: '95%' }}
+          onClick={handleLastEntityClear}
+        >
+          CE
+        </button>
+      </section>
     </div>
   );
 }
